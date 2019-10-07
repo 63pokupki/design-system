65139bf43eb817f18bf08234743e331d4941c583
Dmitry Kuznetsov <x.o.esdes@gmail.com>
2019-10-07 16:23:53 +0400

    Основная карточка закупки закончена

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         ignored: /node_modules/, //исключения в слежении
         poll: 500 //интервал обновления
     },
-    devtool: "eval-source-map", //Инструменты разработчика
+    devtool: "source-map", //Инструменты разработчика
     resolve: {
         alias: {
             //краткие имена путей для импортов
