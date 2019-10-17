af2e53616f34670399e2e2f5ef6dcc3181a24f9a
Ivan <shunyaeviv@63pokupki.ru>
2019-10-17 16:49:27 +0400

    Move scripts in util.js

--- src/js/util.js
+++ src/js/util.js
@@ -21,3 +21,72 @@ if (item_params_list__items) {
         link.addEventListener("click", () => list.classList.toggle("is-expanded"));
     });
 }
+
+// <!-- Модалка копирование позиции -->
+
+    //Раскрытие списка
+    let select = document.querySelectorAll(".modal__select"); //кнопка раскрывающая блок
+    //Перебираем все кнопки
+    for (let bt = 0; bt < select.length; bt++) {
+
+        select[bt].addEventListener("click", function () {
+            this.classList.toggle("is-toggle");
+        });
+    }
+
+    //Выбор позиции
+    (
+        () => {
+            let selectCont = document.querySelectorAll(".modal__select");
+
+            selectCont.forEach((sc) => {
+                let sel_position = sc.querySelector(".modal__selected-position"),
+                    li = sc.querySelectorAll("li.item");
+
+                li.forEach((i) => {
+                    i.addEventListener("click", () => {
+                        sel_position.innerHTML = i.innerHTML;
+                    });
+                });
+            });
+        }
+    )();
+
+
+
+
+// <!-- Карточка товара с коротким изображением -->
+
+    let btn_toggle = document.querySelector(".modal__btn-about-goods"),
+        toggle_txt_container = document.querySelector(".modal__goods-info");
+
+    btn_toggle.addEventListener("click", () => {
+        toggle_txt_container.classList.toggle("is-toggle");
+    });
+
+
+
+// <!-- Кнопки управления количеством товара -->
+
+
+    let minus = document.querySelector(".btn-sum__minus"),
+        plus = document.querySelector(".btn-sum__plus"),
+        input = document.querySelector(".btn-sum__sum-num");
+
+    minus.addEventListener("click", () => {
+        input.value--;
+    });
+    plus.addEventListener("click", () => {
+        input.value++;
+    });
+
+
+
+// <!-- Сердечко -->
+
+    let heart = document.querySelector(".icon-heart-bold");
+
+    let click_heart = () => { heart.classList.toggle("icon-heart-filled"); }
+
+    heart.addEventListener("click", click_heart);
+
