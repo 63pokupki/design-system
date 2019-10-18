5e6d370798d44ff8500f30332378f300ef8b909f
Ivan <shunyaeviv@63pokupki.ru>
2019-10-18 08:17:25 +0400

    Edit

--- src/js/util.js
+++ src/js/util.js
@@ -25,71 +25,75 @@ if (item_params_list__items) {
 
 // <!-- Модалка копирование позиции -->
 
-    //Раскрытие списка
-    let select = document.querySelectorAll(".modal__select"); //кнопка раскрывающая блок
-    //Перебираем все кнопки
-    for (let bt = 0; bt < select.length; bt++) {
+//Раскрытие списка
+let select = document.querySelectorAll(".modal__select"); //кнопка раскрывающая блок
+//Перебираем все кнопки
+for (let bt = 0; bt < select.length; bt++) {
 
-        select[bt].addEventListener("click", function () {
-            this.classList.toggle("is-toggle");
-        });
-    }
+    select[bt].addEventListener("click", function () {
+        this.classList.toggle("is-toggle");
+    });
+}
 
-    //Выбор позиции
-    (
-        () => {
-            let selectCont = document.querySelectorAll(".modal__select");
+//Выбор позиции
+(
+    () => {
+        let selectCont = document.querySelectorAll(".modal__select");
 
-            selectCont.forEach((sc) => {
-                let sel_position = sc.querySelector(".modal__selected-position"),
-                    li = sc.querySelectorAll("li.item");
+        selectCont.forEach((sc) => {
+            let sel_position = sc.querySelector(".modal__selected-position"),
+                li = sc.querySelectorAll("li.item");
 
-                li.forEach((i) => {
-                    i.addEventListener("click", () => {
-                        sel_position.innerHTML = i.innerHTML;
-                    });
+            li.forEach((i) => {
+                i.addEventListener("click", () => {
+                    sel_position.innerHTML = i.innerHTML;
                 });
             });
-        }
-    )();
+        });
+    }
+)();
 
 
 
 
 // <!-- Карточка товара с коротким изображением -->
 
-    let btn_toggle = document.querySelector(".modal__btn-about-goods"),
-        toggle_txt_container = document.querySelector(".modal__goods-info");
+let btn_toggle = document.querySelector(".modal__btn-about-goods"),
+    toggle_txt_container = document.querySelector(".modal__goods-info");
 
+if (btn_toggle) {
     btn_toggle.addEventListener("click", () => {
         toggle_txt_container.classList.toggle("is-toggle");
     });
+}
 
 
 
 // <!-- Кнопки управления количеством товара -->
 
 
-    let minus = document.querySelector(".btn-sum__minus"),
-        plus = document.querySelector(".btn-sum__plus"),
-        input = document.querySelector(".btn-sum__sum-num");
+let minus = document.querySelector(".btn-sum__minus"),
+    plus = document.querySelector(".btn-sum__plus"),
+    input = document.querySelector(".btn-sum__sum-num");
 
-    minus.addEventListener("click", () => {
-        input.value--;
-    });
-    plus.addEventListener("click", () => {
-        input.value++;
-    });
+minus.addEventListener("click", () => {
+    input.value--;
+});
+plus.addEventListener("click", () => {
+    input.value++;
+});
 
 
 
 // <!-- Сердечко -->
 
-    let heart = document.querySelector(".icon-heart-bold");
+let heart = document.querySelector(".icon-heart-bold");
 
-    let click_heart = () => { heart.classList.toggle("icon-heart-filled"); }
+let click_heart = () => {
+    heart.classList.toggle("icon-heart-filled");
+}
 
-    heart.addEventListener("click", click_heart);
+heart.addEventListener("click", click_heart);
 
 
 
@@ -100,6 +104,3 @@ if (collapse_section) {
         heading.addEventListener("click", () => section.classList.toggle("is-expanded"));
     });
 }
-
-
-
