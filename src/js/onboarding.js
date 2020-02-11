e86fa0938b05b73ea928a3fd7a08b66306e304a1
Dmitry Kuznetsov <x.o.esdes@gmail.com>
2020-02-11 17:50:10 +0400

    Стандартный шаблон подсказок, сторонние шаблоны

--- src/js/onboarding.js
+++ src/js/onboarding.js
@@ -2,15 +2,15 @@ const introJs = require("intro.js");
 
 const steps = [
     {
-        intro: `<div class="onboarding-template__content">
-        <div class="onboarding-template__heading">
+        intro: `<div class="onboarding-base-outer__content">
+        <div class="onboarding-base-outer__heading">
             Привет!
         </div>
-        <div class="onboarding-template__heading">
+        <div class="onboarding-base-outer__heading">
             Это наша новая страница закупки.
         </div>
-        <p class="onboarding-template__accent">Что изменилось:</p>
-        <ol class="onboarding-template__list">
+        <p class="onboarding-base-outer__accent">Что изменилось:</p>
+        <ol>
             <li>Товары и описание закупки <b>теперь на одной странице</b></li>
             <li>Теперь <b>нет отдельного списка заказов</b> других участников. Такие товары отмечены синим значком и отображаются в начале каталога</li>
             <li><b>Ряды можно смотреть прямо в карточке товара</b>
@@ -19,13 +19,13 @@ const steps = [
         <p class="semi-bold">Хотите подробнее?</p> 
         </div>`,
         nextLabel: "Посмотреть",
-        tooltipClass: "onboarding-template onboarding-template_start"
+        tooltipClass: "onboarding-base-outer"
     },
     {
         intro: `Все товары закупки расположены сразу под ее описанием. Сначала идут те товары, которые уже заказаны в этом выкупе - на них стоит синий значок
         с количеством заказов.`,
         nextLabel: "Далее",
-        element: document.querySelector('.stock-badge.stock-badge_already')
+        element: document.querySelector(".stock-badge.stock-badge_already")
     },
     {
         intro: `В закупке можно отсортировать товары по популярности, цене, названию, новизне и заполненности рядов. 
@@ -34,21 +34,71 @@ const steps = [
          37 размер, то сверху будут товары,
         где 37 размер в ряду свободен, а сам ряд
         при этом максимально заполнен.`,
-        element: document.querySelector('.ds-popover_sorting.ds-popover'),
+        element: document.querySelector(".ds-popover_sorting.ds-popover"),
         beforeStep: () => {
-            let el = document.querySelector('.ds-popover_sorting.ds-popover');
-            el.style.transition = 'none';
-            
+            let el = document.querySelector(".ds-popover_sorting.ds-popover");
+            el.style.transition = "none";
+
             if (el) {
-                el.classList.add('is-visible')
+                el.classList.add("is-visible");
             }
         }
     },
+    {
+        intro: `По умолчанию показываются товары из всех каталогов закупки. Если хотите выбрать определенный каталог - просто поставьте галочку рядом с ним!`,
+        element: document.querySelector(".stock-d .stock-aside__catalogs")
+    },
+    {
+        intro: `Описание закупки, условия и остальная информация - наверху страницы. Здесь же  Размерная сетка и Отзывы
+        по закупке. Нажав “Показать больше”,
+        вы раскроете полное описание закупки`,
+        element: document.querySelector(".stock-d .stock-tabs__labels")
+    },
+    {
+        intro: `Если возникают вопросы по товарам и условиям закупки - можно написать организатору или перейти к обсуждению закупки на форум.`,
+        element: document.querySelector(
+            ".stock-d .stock-brief__col.stock-brief__right"
+        )
+    },
+    // {
+    //     intro: `Отобразите товары списком и смотрите более подробную информацию.
+    //     В том числе заполненность ряда
+    //     прямо в описании товара.`,
+    //     element: document.querySelector(
+    //         ".stock-d .stock-items-filter__mode .stock-items-filter__list"
+    //     )
+    // },
+    // {
+    //     intro: `Можно выбрать несколько 
+    //     фильтров и после нажатия
+    //     “Применить” останутся только
+    //     подходящие товары!`,
+    //     element: document.querySelector(".stock-d .stock-aside__filters")
+    // },
+    {
+        intro: `Чтобы находить товары быстрее, используйте поиск по закупке.`,
+        element: document.querySelector(
+            ".stock-d .search.stock-items-filter__search"
+        )
+    },
+    {
+        intro: `<div class="onboarding-template__content">
+        <div class="onboarding-template__heading">
+            Теперь вы знаете самое необходимое!
+        </div>
+        <p>Краткий видеообзор по новой странице закупки можно посмотреть <a target="_blank" href="#" class="link link_hover">здесь.</a></p>
+        <span>Бежим заказывать!</span>
+        </div>`,
+        // tooltipClass: "onboarding-template onboarding-template_start",
+        doneLabel: "Перейти к покупкам",
+        beforeStep: () => {
+            fScrollTo();
+        }
+    }
 ];
 
 const DEFAULT_OPTIONS = {
-    highlightClass: "onboarding-template-hightlight",
-    tooltipClass: "onboarding-template",
+    tooltipClass: "onboarding-base-steps",
     skipLabel: "Пропустить",
     doneLabel: "Закончить",
     prevLabel: "Назад",
@@ -58,7 +108,7 @@ const DEFAULT_OPTIONS = {
     showProgress: true,
     showBullets: false,
     showStepNumbers: false,
-    scrollTo: 'tooltip'
+    scrollTo: "tooltip"
 };
 
 window.addEventListener("DOMContentLoaded", fMain);
@@ -74,6 +124,27 @@ function fMain() {
     intro.start();
 
     fInitCloseButton(intro);
+
+    setTimeout(() => fScrollTo(), 500);
+}
+
+/**
+ * Прокрутка
+ * @param {Number} x - координата X прокрутки
+ * @param {Number} y - координата Y прокрутки
+ */
+function fScrollTo(x = 0, y = 0) {
+    let options = {
+        top: x,
+        left: y,
+        behavior: "smooth"
+    };
+
+    try {
+        window.scrollTo(options);
+    } catch {
+        window.scrollTo(x, y);
+    }
 }
 
 /**
@@ -105,14 +176,6 @@ function fUpdatePropertiesStepByStepHook(onboarding, default_options, steps) {
             step.beforeStep();
             onboarding.refresh();
         }
-
-        console.log("onbeforechange");
-    });
-
-    onboarding.onafterchange((item) => {
-       console.log("onafterchange");
-       onboarding.refresh();
-       console.dir(item);
     });
 }
 
@@ -130,7 +193,7 @@ function fInitCloseButton(onboarding) {
     // создание кнопки
     let close = document.createElement("button");
     close.append(icon);
-    close.className = `${DEFAULT_OPTIONS.tooltipClass}__close`;
+    close.className = `introjs-tooltip__close`;
     close.addEventListener("click", () => onboarding.exit(true));
 
     // добавление кнопки
