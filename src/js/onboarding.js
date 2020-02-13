76f3e8440ddf384a42c2281ee3e9a2f5db3fdda4
Dmitry Kuznetsov <x.o.esdes@gmail.com>
2020-02-13 16:20:25 +0400

    Закончена база по подсказкам

--- src/js/onboarding.js
+++ src/js/onboarding.js
@@ -1,61 +1,156 @@
 const introJs = require("intro.js");
 
 // мобильные шаги
+// const steps = [
+//     {
+//         intro: `<div class="onboarding-content">
+//             <div class="onboarding-heading">
+//             Привет! Это наша новая страница закупки.
+//             </div>
+//             <p class="onboarding-accent">Что изменилось:</p>
+//             <ol>
+//                 <li>Товары и описание закупки теперь на одной странице</li>
+//                 <li>Теперь нет отдельного списка заказов других участников. Такие товары отмечены синим значком и отображаются в начале каталога</li>
+//                 <li style="margin-right: 50px">Ряды можно смотреть прямо в карточке товара
+//                     и делать сортировку по заполненности рядов</li>
+//             </ol>
+//             <p class="semi-bold">Хотите подробнее?</p>
+//         </div>`,
+//         nextLabel: "Посмотреть",
+//         tooltipClass: "onboarding-base-outer"
+//     },
+//     {
+//         intro: `Здесь вся информация
+//         о закупке, Размерная сетка,
+//         а также Отзывы.`,
+//         element: getElement(".stock-m-info__section-collapse, .stock-m-info__tabs")
+//     },
+//     {
+//         intro: `Можно задать вопрос
+//         организатору или перейти
+//         к обсуждению закупки на форум.`,
+//         element: getElement(".stock-m-info__buttons")
+//     },
+//     {
+//         intro: `По умолчанию показаны
+//         товары из всех каталогов.
+//         Отметьте нужные каталоги.`,
+//         element: getElement(".stock-m-control__catalogs-btn")
+//     },
+//     {
+//         intro: `Поиск по закупке поможет
+//         находить товары быстрее.`,
+//         element: getElement(".stock-m-control__search-m, .stock-m-control__search-t")
+//     },
+//     {
+//         intro: `Отобразите товары по одному
+//         и смотрите заполнение ряда
+//         прямо в описании товара.`,
+//         element: getElement(".stock-m-control__buttons")
+//     },
+//     {
+//         intro: `Выберите нужный размер и
+//         сортируйте по заполненности рядов.
+//         Сверху будут подходящие товары.`,
+//         element: getElement(".stock-m__filters")
+//     },
+//     {
+//         intro: `<div class="onboarding-content" style="font-size: 14px; margin-bottom: 15px">
+//         <div class="onboarding-heading">
+//             Теперь вы знаете самое необходимое!
+//         </div>
+//         <p>Краткий видеообзор по новой странице закупки можно посмотреть <a target="_blank" href="#" class="link link_hover">здесь.</a></p>
+//         <span>Бежим заказывать!</span>
+//         </div>`,
+//         doneLabel: "Перейти к покупкам",
+//         beforeStep: () => {
+//             fScrollTo();
+//         }
+//     }
+// ];
+
+// десктоп шаги
 const steps = [
     {
         intro: `<div class="onboarding-content">
-            <div class="onboarding-heading">
-            Привет! Это наша новая страница закупки.
-            </div>
-            <p class="onboarding-accent">Что изменилось:</p>
-            <ol>
-                <li>Товары и описание закупки теперь на одной странице</li>
-                <li>Теперь нет отдельного списка заказов других участников. Такие товары отмечены синим значком и отображаются в начале каталога</li>
-                <li style="margin-right: 50px">Ряды можно смотреть прямо в карточке товара
-                    и делать сортировку по заполненности рядов</li>
-            </ol>
-            <p class="semi-bold">Хотите подробнее?</p>
+        <div class="onboarding-heading">
+            Привет!
+        </div>
+        <div class="onboarding-heading">
+            Это наша новая страница закупки.
+        </div>
+        <p class="onboarding-accent">Что изменилось:</p>
+        <ol>
+            <li>Товары и описание закупки <b>теперь на одной странице</b></li>
+            <li>Теперь <b>нет отдельного списка заказов</b> других участников. Такие товары отмечены синим значком и отображаются в начале каталога</li>
+            <li><b>Ряды можно смотреть прямо в карточке товара</b>
+                и делать сортировку по заполненности рядов</b></li>
+        </ol>
+        <p class="semi-bold">Хотите подробнее?</p>
         </div>`,
         nextLabel: "Посмотреть",
         tooltipClass: "onboarding-base-outer"
     },
     {
-        intro: `Здесь вся информация
-        о закупке, Размерная сетка,
-        а также Отзывы.`,
-        element: getElement(".stock-m .zzz")
+        intro: `Все товары закупки расположены сразу под ее описанием. Сначала идут те товары, которые уже заказаны в этом выкупе - на них стоит синий значок
+        с количеством заказов.`,
+        nextLabel: "Далее",
+        element: getElement(".stock-badge.stock-badge_already")
     },
     {
-        intro: `Можно задать вопрос
-        организатору или перейти
-        к обсуждению закупки на форум.`,
-        element: getElement(".stock-m-info__buttons")
+        intro: `В закупке можно отсортировать товары по популярности, цене, названию, новизне и заполненности рядов.
+        <br><br>
+        Например, если отсортировать по заполненности рядов и выбрать в фильтре
+         37 размер, то сверху будут товары,
+        где 37 размер в ряду свободен, а сам ряд
+        при этом максимально заполнен.`,
+        element: getElement(".ds-popover_sorting.ds-popover"),
+        beforeStep: () => {
+            try {
+                let el = document.querySelector(
+                    ".ds-popover_sorting.ds-popover"
+                );
+                el.classList.add("disable-transition");
+
+                if (el) {
+                    el.classList.add("is-visible");
+                }
+            } catch (e) {}
+        }
     },
     {
-        intro: `По умолчанию показаны
-        товары из всех каталогов.
-        Отметьте нужные каталоги.`,
-        element: getElement(".stock-m-control__catalogs-btn")
+        intro: `По умолчанию показываются товары из всех каталогов закупки. Если хотите выбрать определенный каталог - просто поставьте галочку рядом с ним!`,
+        element: getElement(".stock-d .stock-aside__catalogs")
     },
     {
-        intro: `Поиск по закупке поможет
-        находить товары быстрее.`,
-        element: getElement(".stock-m-control__search-m, .stock-m-control__search-t")
+        intro: `Описание закупки, условия и остальная информация - наверху страницы. Здесь же  Размерная сетка и Отзывы
+        по закупке. Нажав “Показать больше”,
+        вы раскроете полное описание закупки`,
+        element: getElement(".stock-d .stock-tabs__labels")
     },
     {
-        intro: `Отобразите товары по одному
-        и смотрите заполнение ряда
+        intro: `Если возникают вопросы по товарам и условиям закупки - можно написать организатору или перейти к обсуждению закупки на форум.`,
+        element: getElement(".stock-d .stock-brief__col.stock-brief__right")
+    },
+    {
+        intro: `Отобразите товары списком и смотрите более подробную информацию.
+        В том числе заполненность ряда
         прямо в описании товара.`,
-        element: getElement(".stock-m-control__buttons")
+        element: getElement(".stock-d .stock-items-filter__list")
     },
     {
-        intro: `Выберите нужный размер и
-        сортируйте по заполненности рядов.
-        Сверху будут подходящие товары.`,
-        element: getElement(".stock-m__filters")
+        intro: `Можно выбрать несколько
+        фильтров и после нажатия
+        “Применить” останутся только
+        подходящие товары!`,
+        element: getElement(".stock-d .stock-aside__filters")
     },
     {
-        intro: `<div class="onboarding-content" style="font-size: 14px; margin-bottom: 15px">
+        intro: `Чтобы находить товары быстрее, используйте поиск по закупке.`,
+        element: getElement(".stock-d .search.stock-items-filter__search")
+    },
+    {
+        intro: `<div class="onboarding-content">
         <div class="onboarding-heading">
             Теперь вы знаете самое необходимое!
         </div>
@@ -63,113 +158,12 @@ const steps = [
         <span>Бежим заказывать!</span>
         </div>`,
         doneLabel: "Перейти к покупкам",
+        tooltipClass: "onboarding-base-outer",
         beforeStep: () => {
             fScrollTo();
         }
     }
 ];
-
-// десктоп шаги
-// const steps = [
-//     {
-//         intro: `<div class="onboarding-base-outer__content">
-//         <div class="onboarding-base-outer__heading">
-//             Привет!
-//         </div>
-//         <div class="onboarding-base-outer__heading">
-//             Это наша новая страница закупки.
-//         </div>
-//         <p class="onboarding-base-outer__accent">Что изменилось:</p>
-//         <ol>
-//             <li>Товары и описание закупки <b>теперь на одной странице</b></li>
-//             <li>Теперь <b>нет отдельного списка заказов</b> других участников. Такие товары отмечены синим значком и отображаются в начале каталога</li>
-//             <li><b>Ряды можно смотреть прямо в карточке товара</b>
-//                 и делать сортировку по заполненности рядов</b></li>
-//         </ol>
-//         <p class="semi-bold">Хотите подробнее?</p>
-//         </div>`,
-//         nextLabel: "Посмотреть"
-//         // tooltipClass: "onboarding-base-outer"
-//     },
-//     {
-//         intro: `Все товары закупки расположены сразу под ее описанием. Сначала идут те товары, которые уже заказаны в этом выкупе - на них стоит синий значок
-//         с количеством заказов.`,
-//         nextLabel: "Далее",
-//         element: document.querySelector(".stock-badge.stock-badge_already")
-//     },
-//     {
-//         intro: `В закупке можно отсортировать товары по популярности, цене, названию, новизне и заполненности рядов.
-//         <br><br>
-//         Например, если отсортировать по заполненности рядов и выбрать в фильтре
-//          37 размер, то сверху будут товары,
-//         где 37 размер в ряду свободен, а сам ряд
-//         при этом максимально заполнен.`,
-//         element: document.querySelector(".ds-popover_sorting.ds-popover"),
-//         beforeStep: () => {
-//             try {
-//                 let el = document.querySelector(
-//                     ".ds-popover_sorting.ds-popover"
-//                 );
-//                 el.classList.add("disable-transition");
-
-//                 if (el) {
-//                     el.classList.add("is-visible");
-//                 }
-//             } catch (e) {}
-//         }
-//     },
-//     {
-//         intro: `По умолчанию показываются товары из всех каталогов закупки. Если хотите выбрать определенный каталог - просто поставьте галочку рядом с ним!`,
-//         element: document.querySelector(".stock-d .stock-aside__catalogs")
-//     },
-//     {
-//         intro: `Описание закупки, условия и остальная информация - наверху страницы. Здесь же  Размерная сетка и Отзывы
-//         по закупке. Нажав “Показать больше”,
-//         вы раскроете полное описание закупки`,
-//         element: document.querySelector(".stock-d .stock-tabs__labels")
-//     },
-//     {
-//         intro: `Если возникают вопросы по товарам и условиям закупки - можно написать организатору или перейти к обсуждению закупки на форум.`,
-//         element: document.querySelector(
-//             ".stock-d .stock-brief__col.stock-brief__right"
-//         )
-//     },
-//     {
-//         intro: `Отобразите товары списком и смотрите более подробную информацию.
-//         В том числе заполненность ряда
-//         прямо в описании товара.`,
-//         element: document.querySelector(
-//             ".stock-d .stock-items-filter__mode .stock-items-filter__list"
-//         )
-//     },
-//     {
-//         intro: `Можно выбрать несколько
-//         фильтров и после нажатия
-//         “Применить” останутся только
-//         подходящие товары!`,
-//         element: document.querySelector(".stock-d .stock-aside__filters")
-//     },
-//     {
-//         intro: `Чтобы находить товары быстрее, используйте поиск по закупке.`,
-//         element: document.querySelector(
-//             ".stock-d .search.stock-items-filter__search"
-//         )
-//     },
-//     {
-//         intro: `<div class="onboarding-base-outer__content">
-//         <div class="onboarding-base-outer__heading">
-//             Теперь вы знаете самое необходимое!
-//         </div>
-//         <p>Краткий видеообзор по новой странице закупки можно посмотреть <a target="_blank" href="#" class="link link_hover">здесь.</a></p>
-//         <span>Бежим заказывать!</span>
-//         </div>`,
-//         doneLabel: "Перейти к покупкам",
-//         // tooltipClass: "onboarding-base-outer",
-//         beforeStep: () => {
-//             fScrollTo();
-//         }
-//     }
-// ];
 
 const DEFAULT_OPTIONS = {
     tooltipClass: "onboarding-base-steps",
