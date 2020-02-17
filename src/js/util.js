6c40d1eaf4b6d80c2e97af59df3fcac68268dc3b
Ivan Schunyaev <32461603+SteveGerr@users.noreply.github.com>
2020-02-17 09:56:16 +0400

    Design system ivan (#264)
    * Add timer, add promo page
    
    * Add classes pagination

--- src/js/util.js
+++ src/js/util.js
@@ -377,3 +377,39 @@ if (pageStockOrg) {
     });
 }
 
+// Timer reverse
+function timeRem (et) {
+
+    let t = Date.parse(et) - Date.parse(new Date()),
+        days = Math.floor(t / (1000 * 60 * 60 * 24));
+
+    return {
+        total: t,
+        days: days
+    }
+}
+function initClock (et) {
+    let timer   = document.querySelectorAll(".timer-reverse");
+
+    if (timer) {
+        timer.forEach((tm) => {
+            let elDays = tm.querySelector("#days");            
+
+            function updateClock() {
+                let t = timeRem(et);
+            
+                elDays.innerHTML = t.days;
+            
+                if (t.total <= 0) {
+                  clearInterval(timeinterval);
+                }
+              }
+              updateClock();
+            //   Обновляем каждые сутки (1 день = 86400 секунд)
+              let timeinterval = setInterval(updateClock, 86400);
+        })        
+    }
+}
+let deadline = "March 02 2020 00:00:00 GMT+0400";
+initClock(deadline);
+
