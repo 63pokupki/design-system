996ea4a206a9afb6e10abbf24fc6b362c5d78424
Dmitry Kuznetsov <x.o.esdes@gmail.com>
2020-07-31 14:25:10 +0400

    Добавлен компонент ItemBadge

---
+++ src/components/ItemBadge/ItemBadge.vue
@@ -0,0 +1,33 @@
+<template>
+  <span class="ItemBadge" :class="[_color, _pos]">
+    <slot></slot>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "ItemBadge",
+  props: {
+    'color': {
+      type: String,
+      default: 'primary'
+    },
+    "pos": {
+      type: String,
+      default: "left-top"
+    }
+  },
+  computed: {
+    _color() {
+      return 'ItemBadge_' + this.color;
+    },
+    _pos() {
+      return 'ItemBadge_' + this.pos;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./ItemBadge.scss";
+</style>
