804703a0c9eb0e3e18be3595afa72a8a5bc47265
kuznetsovdo <x.o.esdes@gmail.com>
2020-08-20 16:09:41 +0400

    Компонент switcher

---
+++ src/components/Switcher/Switcher.stories.js
@@ -0,0 +1,66 @@
+import Switcher from "./Switcher.vue";
+
+export default {
+    title: "Компоненты/Switcher",
+    component: Switcher
+};
+
+export const accent = () => ({
+    components: { Switcher },
+    data() {
+        return {
+            checked: false
+        }
+    },
+    template: "<Switcher v-model='checked' type='accent'></Switcher>"
+});
+
+export const accentDisabled = () => ({
+    components: { Switcher },
+    data() {
+        return {
+            checked: false
+        }
+    },
+    template: "<Switcher v-model='checked' type='accent' disabled></Switcher>"
+});
+
+export const outline = () => ({
+    components: { Switcher },
+    data() {
+        return {
+            checked: false
+        }
+    },
+    template: "<Switcher v-model='checked' type='outline'></Switcher>"
+});
+
+export const outlineDisabled = () => ({
+    components: { Switcher },
+    data() {
+        return {
+            checked: false
+        }
+    },
+    template: "<Switcher v-model='checked' type='outline' disabled></Switcher>"
+});
+
+export const left = () => ({
+    components: { Switcher },
+    data() {
+        return {
+            checked: false
+        }
+    },
+    template: "<Switcher v-model='checked' position='left'>Switcher слева</Switcher>"
+});
+
+export const right = () => ({
+    components: { Switcher },
+    data() {
+        return {
+            checked: false
+        }
+    },
+    template: "<Switcher v-model='checked' position='right'>Switcher справа</Switcher>"
+});
