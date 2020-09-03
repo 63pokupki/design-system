59ec44c5b74b9e720995b61ac0c309d8a4090b43
Dmitriy Kuznetsov <40758279+nullilac@users.noreply.github.com>
2020-09-03 14:57:34 +0400

    Компонент тега поиска (#612)

---
+++ src/components/specific/BadgeCategoryTag/BadgeCategoryTag.vue
@@ -0,0 +1,33 @@
+<template functional>
+    <div
+        v-on="listeners"
+        :style="props.styleObj"
+        class="spui-BadgeCategoryTag"
+        :class="[`spui-BadgeCategoryTag_${props.size}`]"
+    >
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "BadgeCategoryTag",
+    props: {
+        size: {
+            type: String,
+            default: "sm",
+            validator: function (value) {
+                return ["sm", "lg"].indexOf(value) != -1;
+            },
+        },
+        styleObj: {
+            type: Object,
+            default: () => {},
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./BadgeCategoryTag.scss";
+</style>
