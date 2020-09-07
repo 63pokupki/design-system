20833c467db10548b3d413eef634d6c58af0f944
kuznetsovdo <x.o.esdes@gmail.com>
2020-09-07 17:40:46 +0400

    Вылив компонентов сторибука

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,34 @@
+import BtnFavorite from "./common/BtnFavorite/BtnFavorite.vue";
+import Button from "./common/Button/Button.vue";
+import Checkbox from "./common/Checkbox/Checkbox.vue";
+import Collapse from "./common/Collapse/Collapse.vue";
+import DropdownModalWithSelect from "./common/DropdownModalWithSelect/DropdownModalWithSelect.vue";
+import DropdownTooltipWithSelect from "./common/DropdownTooltipWithSelect/DropdownTooltipWithSelect.vue";
+import ImageSwitch from "./common/ImageSwitch/ImageSwitch.vue";
+import ItemBadge from "./common/ItemBadge/ItemBadge.vue";
+import Modal from "./common/Modal/Modal.vue";
+import RangePrice from "./common/RangePrice/RangePrice.vue";
+import Sidebar from "./common/Sidebar/Sidebar.vue";
+import Switcher from "./common/Switcher/Switcher.vue";
+import Tooltip from "./common/Tooltip/Tooltip.vue";
+/* Specific*/
+import BadgeCategoryTag from "./specific/BadgeCategoryTag/BadgeCategoryTag.vue";
+import CollapseMultipleSelectList from "./specific/CollapseMultipleSelectList/CollapseMultipleSelectList.vue";
 
+export {
+    BtnFavorite,
+    Button,
+    Checkbox,
+    Collapse,
+    DropdownModalWithSelect,
+    DropdownTooltipWithSelect,
+    ImageSwitch,
+    ItemBadge,
+    Modal,
+    RangePrice,
+    Sidebar,
+    Switcher,
+    Tooltip,
+    BadgeCategoryTag,
+    CollapseMultipleSelectList,
+};
