55a7e65f095a14ff477d2141bb9330db6d35bfb8
kuznetsovdo <x.o.esdes@gmail.com>
2020-09-17 10:30:08 +0400

    Поправлены пропсы для компонента Сортировка по

--- src/components/common/DropdownTooltipWithSelect/DropdownTooltipWithSelect.stories.js
+++ src/components/common/DropdownTooltipWithSelect/DropdownTooltipWithSelect.stories.js
@@ -9,16 +9,41 @@ export const standart = () => ({
     components: { DropdownTooltipWithSelect },
     data() {
         return {
-            value: { key: {label: "популярности"}, value: "0" },
+            value: { key: { label: "популярности" }, value: "0" },
             values: [
-                { key: {label: "популярности"}, value: "0" },
-                { key: {label: "возрастанию цены"}, value: "0" },
-                { key: {label: "убыванию цены"}, value: "0" },
-                { key: {label: "новизне"}, value: "0" },
-                { key: {label: "названию"}, value: "0" },
-                { key: {label: "заполненности рядов"}, value: "0" },
+                { key: { label: "популярности" }, value: "0" },
+                { key: { label: "возрастанию цены" }, value: "0" },
+                { key: { label: "убыванию цены" }, value: "0" },
+                { key: { label: "новизне" }, value: "0" },
+                { key: { label: "названию" }, value: "0" },
+                { key: { label: "заполненности рядов" }, value: "0" },
             ],
-            label: (value) => value.key.label
+            label: (value) => value.key.label,
+        };
+    },
+    template: `
+            <DropdownTooltipWithSelect 
+            v-model="value" 
+            :values="values"
+            :label="label"
+            heading="cортировка по">
+            </DropdownTooltipWithSelect>`,
+});
+
+export const objectValues = () => ({
+    components: { DropdownTooltipWithSelect },
+    data() {
+        return {
+            value: { key: { label: "популярности" }, value: "0" },
+            values: {
+                0: { key: { label: "популярности" }, value: "0" },
+                1: { key: { label: "возрастанию цены" }, value: "0" },
+                2: { key: { label: "убыванию цены" }, value: "0" },
+                3: { key: { label: "новизне" }, value: "0" },
+                4: { key: { label: "названию" }, value: "0" },
+                5: { key: { label: "заполненности рядов" }, value: "0" },
+            },
+            label: (value) => value.key.label,
         };
     },
     template: `
