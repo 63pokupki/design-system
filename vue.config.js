5f83687ab21f75e888f485858c33f2415c6cb2df
kuznetsovdo <x.o.esdes@gmail.com>
2020-10-07 11:29:06 +0400

    Костыль с проблемой резолвинга картинок после сборки в таргете либы

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,51 @@
-const path = require('path');
+const path = require("path");
+const svgToMiniDataURI = require("mini-svg-data-uri");
 
-const vueSrc = './src';
+const vueSrc = "./src";
 module.exports = {
-  css: {
-    loaderOptions: {
-      sass: {
-        additionalData: '@import "@/styles/utils/_variables.scss";@import "@/styles/utils/_mixins.scss";@import "@/styles/utils/_extends.scss";@import "@/fonts/icomoon/icomoon-font.scss";@import "@/fonts/opensans/opensans-font.scss";@import "@/components/index.scss";',
-      },
+    css: {
+        loaderOptions: {
+            sass: {
+                additionalData:
+                    '@import "@/styles/utils/_variables.scss";@import "@/styles/utils/_mixins.scss";@import "@/styles/utils/_extends.scss";@import "@/fonts/icomoon/icomoon-font.scss";@import "@/fonts/opensans/opensans-font.scss";@import "@/components/index.scss";',
+            },
+        },
     },
-  },
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, vueSrc),
-        // краткие имена путей для импортов
-        styles: path.resolve(__dirname, 'src/styles'),
-        images: path.resolve(__dirname, 'src/images'),
-        icons: path.resolve(__dirname, 'src/icons'),
-        fonts: path.resolve(__dirname, 'src/fonts'),
-      },
-      extensions: ['.js', '.vue', '.json'],
+    chainWebpack: (config) => {
+        config.module
+            .rule("images")
+            .use("url-loader")
+            .loader("url-loader")
+            .tap((options) => {
+                return {
+                    limit: true,
+                    name: 'img/[name].[hash:8].[ext]',
+                };
+            });
+
+        config.module
+            .rule("svg")
+            .use("file-loader")
+            .loader("url-loader")
+            .tap((options) => {
+                return {
+                    limit: 10240,
+                    name: 'img/[name].[hash:8].[ext]',
+                    generator: (content) => svgToMiniDataURI(content.toString()),
+                };
+            });
     },
-  },
+    configureWebpack: {
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, vueSrc),
+                // краткие имена путей для импортов
+                styles: path.resolve(__dirname, "src/styles"),
+                images: path.resolve(__dirname, "src/images"),
+                icons: path.resolve(__dirname, "src/icons"),
+                fonts: path.resolve(__dirname, "src/fonts"),
+            },
+            extensions: [".js", ".vue", ".json"],
+        },
+    },
 };
