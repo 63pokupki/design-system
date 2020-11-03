a3b4fd63b681151eb9864deae05672619b665693
Dmitriy Kuznetsov <40758279+nullilac@users.noreply.github.com>
2020-11-03 11:54:23 +0400

    Добавлен компонент ScrollIntoElementButtons (#649)

---
+++ src/components/common/ScrollIntoElementButtons/ScrollIntoElementButtons.stories.js
@@ -0,0 +1,19 @@
+import ScrollIntoElementButtons from "./ScrollIntoElementButtons.vue";
+
+export default {
+    title: "Общие/ScrollIntoElementButtons",
+    component: ScrollIntoElementButtons,
+};
+
+export const standart = () => ({
+    components: { ScrollIntoElementButtons },
+    data() {
+        return {};
+    },
+    template: `
+    <div>
+        <div style="height: 2000px; background: green;"></div>
+       <ScrollIntoElementButtons target-up-id="root" target-down-id="root" ></ScrollIntoElementButtons>
+    </div>
+    `,
+});
