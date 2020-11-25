53ffa5b674212cfd9aeb938fc9f15a7ecace18a0
nullilac <x.o.esdes@gmail.com>
2020-11-25 15:59:11 +0400

    Добавлена проверка a11i

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -1,24 +1,25 @@
 module.exports = {
-  stories: ['../../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: [
-    '@storybook/addon-actions',
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        babelOptions: {
-          presets: [
-            [
-              '@vue/cli-plugin-babel/preset',
-              {
-                jsx: false
-              }
-            ]
-          ]
-        }
-      }
-    },
-    '@storybook/addon-knobs/register',
-    '@storybook/addon-links/register',
-    '@storybook/addon-backgrounds/register'
-  ]
-}
+    stories: ['../../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+    addons: [
+        '@storybook/addon-actions',
+        {
+            name: '@storybook/addon-docs',
+            options: {
+                babelOptions: {
+                    presets: [
+                        [
+                            '@vue/cli-plugin-babel/preset',
+                            {
+                                jsx: false,
+                            },
+                        ],
+                    ],
+                },
+            },
+        },
+        '@storybook/addon-knobs/register',
+        '@storybook/addon-links/register',
+        '@storybook/addon-backgrounds/register',
+        '@storybook/addon-a11y',
+    ],
+};
--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,12 +1,13 @@
-import { addParameters } from "@storybook/vue";
+/* eslint-disable import/no-extraneous-dependencies */
+import { addParameters } from '@storybook/vue';
 
 addParameters({
     backgrounds: {
-        default: "white",
+        default: 'white',
         values: [
-            { name: "white", value: "#ffffff" },
-            { name: "light", value: "#f2f2f3" },
-            { name: "dark", value: "#171717" },
+            { name: 'white', value: '#ffffff' },
+            { name: 'light', value: '#f2f2f3' },
+            { name: 'dark', value: '#171717' },
         ],
     },
 });
