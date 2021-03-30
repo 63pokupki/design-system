02ff4c4a90189a7ebe103b016f5a882ee6f62164
Dmitriy Kuznetsov <40758279+nullilac@users.noreply.github.com>
2021-03-30 11:19:18 +0400

    Support 1122 (#677)
    * Обновлена версия storybook
    
    * В строку поиска добавлены бренды

--- src/components/specific/InputSearchWithHints/InputSearchWithHints.stories.js
+++ src/components/specific/InputSearchWithHints/InputSearchWithHints.stories.js
@@ -16,30 +16,44 @@ export const standart = () => ({
             isCategoryOpen: false,
             hints: [
                 {
+                    id: 104,
                     label: 'Красное платье',
                     count: 2345,
                 },
                 {
+                    id: 105,
                     label:
                         'Вечернее платье на выпускной и очень длинное название подсказки',
                     count: 345830,
                 },
                 {
+                    id: 177,
                     label: 'Свадебное платье',
                     count: 7462089674,
                 },
                 {
+                    id: 178,
                     label: 'Летнее платье',
                     count: 3,
                 },
             ],
+            brands: [
+                { id: 364613, brand_name: 'Платья Ксении Верд' },
+                { id: 147416, brand_name: 'Платина' },
+                { id: 347930, brand_name: 'Платиновая лаборатория' },
+            ],
             fnHintLabel: (hint) => hint.label,
             fnHintCount: (hint) => hint.count,
+            fnBrandLabel: (brand) => brand.brand_name,
+            fnBrandUniqKey: (brand) => brand.id,
         };
     },
     methods: {
         onClickHint(hint) {
-            this.text = this.fnHintLabel(hint);
+            this.text = this.fnBrandLabel(hint);
+        },
+        onClickBrand(brand) {
+            this.text = this.fnHintLabel(brand);
         },
         onHintsOpenChange(value) {
             this.isHintsOpen = value;
@@ -54,6 +68,19 @@ export const standart = () => ({
             console.log('Отправка запроса');
         },
     },
-    template:
-        '<InputSearchWithHints v-model="text" :hints="hints" :is-hints-open="isHintsOpen" :is-category-open="isCategoryOpen" @hint-click="onClickHint" @hints-open-change="onHintsOpenChange" @category-open-change="onCategoryOpenChange" @input-by-items="onInputByItems" @search="onSearch"></InputSearchWithHints>',
+    template: `<InputSearchWithHints 
+        v-model="text" 
+        :hints="hints"
+        :is-hints-open="isHintsOpen" 
+        :is-category-open="isCategoryOpen"
+        :fn-hint-label="fnHintLabel"
+        :fn-hint-count="fnHintCount"
+        :fn-brand-label="fnBrandLabel"
+        :fn-brand-uniq-key="fnBrandUniqKey"
+        @hint-click="onClickHint" 
+        @brand-click="onClickHint" 
+        @hints-open-change="onHintsOpenChange" 
+        @category-open-change="onCategoryOpenChange" 
+        @input-by-items="onInputByItems" 
+        @search="onSearch"></InputSearchWithHints>`,
 });
